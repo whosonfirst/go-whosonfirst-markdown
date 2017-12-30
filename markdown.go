f1f260d02561234c2337d7b4052be880b34e69d4
thisisaaroland <aaron@mapzen.com>
2017-12-30 15:20:46 -0800

    make it basically work

--- markdown.go
+++ markdown.go
@@ -3,7 +3,7 @@ package markdown
 import (
 	"bufio"
 	"bytes"
-	"github.com/microcosm-cc/bluemonday"
+	// "github.com/microcosm-cc/bluemonday"
 	"gopkg.in/russross/blackfriday.v2"
 	"io"
 	_ "log"
@@ -12,6 +12,46 @@ import (
 
 type nopCloser struct {
 	io.Reader
+}
+
+type WOFRenderer struct {
+	bf   *blackfriday.HTMLRenderer
+	meta map[string]string
+}
+
+func (r *WOFRenderer) RenderNode(w io.Writer, node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
+
+	switch node.Type {
+
+	case blackfriday.Image:
+		return r.bf.RenderNode(w, node, entering)
+	default:
+		return r.bf.RenderNode(w, node, entering)
+	}
+}
+
+func (r *WOFRenderer) RenderHeader(w io.Writer, ast *blackfriday.Node) {
+
+	// please replace me with proper templates
+
+	r.bf.RenderHeader(w, ast)
+
+	w.Write([]byte(`<style type="text/css">
+		body { font-family: serif; margin:2em; margin-left: 10%; margin-right: 10%; font-size: 1.3em; line-height: 1.5em; }
+		img { max-width: 640px !important; max-height: 480px !important; border: 1px dotted #ccc; padding: .5em; margin: 0 auto; margin-bottom:1em; margin-top: 1em; }
+	</style> `))
+
+	_, ok := r.meta["title"]
+
+	if ok {
+		w.Write([]byte("<h2>" + r.meta["title"] + "</h2>"))
+	}
+}
+
+func (r *WOFRenderer) RenderFooter(w io.Writer, ast *blackfriday.Node) {
+
+	// please replace me with proper templates
+	r.bf.RenderFooter(w, ast)
 }
 
 func (nopCloser) Close() error { return nil }
@@ -60,22 +100,34 @@ func ToHTML(md io.ReadCloser) (io.ReadCloser, error) {
 	body := []byte(post)
 
 	flags := blackfriday.CommonHTMLFlags
-	flags |= blackfriday.SkipHTML
-	flags |= blackfriday.SkipLinks
-	flags |= blackfriday.SkipImages
 	flags |= blackfriday.CompletePage
 	flags |= blackfriday.UseXHTML
 
+	title := ""
+
+	_, ok := meta["title"]
+
+	if ok {
+		title = meta["title"]
+	}
+
 	params := blackfriday.HTMLRendererParameters{
 		Flags: flags,
+		Title: title,
 	}
-	
+
 	renderer := blackfriday.NewHTMLRenderer(params)
-	unsafe := blackfriday.Run(body, blackfriday.WithRenderer(renderer))
 
-	safe := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+	r := WOFRenderer{
+		bf:   renderer,
+		meta: meta,
+	}
 
-	html := bytes.NewReader(safe)
+	unsafe := blackfriday.Run(body, blackfriday.WithRenderer(&r))
+
+	// safe := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
+
+	html := bytes.NewReader(unsafe)
 	return nopCloser{html}, nil
 
 }
