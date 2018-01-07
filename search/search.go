402e66bc25a64f3c7b6ae7b13437bb5bcc0f05d2
thisisaaronland <whosonfirst@mapzen.com>
2018-01-07 09:01:01 -0800

    first cut at wiring in better RenderNode; compiles but not tested and probably doesn't work

--- search/search.go
+++ search/search.go
@@ -4,63 +4,11 @@ import (
 	"github.com/whosonfirst/go-whosonfirst-markdown"
 	"gopkg.in/russross/blackfriday.v2"
 	"io"
-	"log"
+	_ "log"
 )
 
-type SearchRenderer struct {
-	bf  *blackfriday.HTMLRenderer
-	doc SearchDocument
-}
-
-/*
-
-   Document NodeType = iota
-   BlockQuote
-   List
-   Item
-   Paragraph
-   Heading
-   HorizontalRule
-   Emph
-   Strong
-   Del
-   Link
-   Image
-   Text
-   HTMLBlock
-   CodeBlock
-   Softbreak
-   Hardbreak
-   Code
-   HTMLSpan
-   Table
-   TableCell
-   TableHead
-   TableBody
-   TableRow
-
-*/
-
-func (r *SearchRenderer) RenderNode(w io.Writer, node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
-
-	log.Println("TYPE", node.Type, node.String())
-
-	switch node.Type {
-
-	case blackfriday.Image:
-		return r.bf.RenderNode(w, node, entering)
-	default:
-		return r.bf.RenderNode(w, node, entering)
-	}
-}
-
-func (r *SearchRenderer) RenderHeader(w io.Writer, ast *blackfriday.Node) {
-	return
-}
-
-func (r *SearchRenderer) RenderFooter(w io.Writer, ast *blackfriday.Node) {
-	// write to search index here...
-	return
+type Indexer interface {
+	IndexDocument(doc *markdown.Document) (*SearchDocument, error)
 }
 
 type SearchDocument struct {
@@ -68,7 +16,9 @@ type SearchDocument struct {
 	Authors []string
 	Date    string
 	Links   []string
-	Body    string
+	Body    []string
+	Code    []string
+	Images  []string
 }
 
 func NewSearchDocument(doc *markdown.Document) (*SearchDocument, error) {
@@ -81,7 +31,9 @@ func NewSearchDocument(doc *markdown.Document) (*SearchDocument, error) {
 		Authors: fm.Authors,
 		Date:    "",
 		Links:   []string{},
-		Body:    "",
+		Body:    []string{},
+		Code:    []string{},
+		Images:  []string{},
 	}
 
 	params := blackfriday.HTMLRendererParameters{}
@@ -97,6 +49,93 @@ func NewSearchDocument(doc *markdown.Document) (*SearchDocument, error) {
 	return &search_doc, nil
 }
 
-type Indexer interface {
-	IndexDocument(doc *markdown.Document) (*SearchDocument, error)
+type SearchRenderer struct {
+	bf  *blackfriday.HTMLRenderer
+	doc SearchDocument
+}
+
+func (r *SearchRenderer) RenderNode(w io.Writer, node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
+
+	str_value := string(node.Literal)
+
+	switch node.Type {
+	case blackfriday.Text:
+
+		if node.Parent.Type == blackfriday.Link {
+			r.doc.Links = append(r.doc.Links, str_value)
+		} else {
+			r.doc.Body = append(r.doc.Body, str_value)
+		}
+
+	case blackfriday.Softbreak:
+		// pass
+	case blackfriday.Hardbreak:
+		// pass
+	case blackfriday.Emph:
+		// pass
+	case blackfriday.Strong:
+		// pass
+	case blackfriday.Del:
+		// pass
+	case blackfriday.HTMLSpan:
+		r.doc.Body = append(r.doc.Body, str_value)
+	case blackfriday.Link:
+		str_dest := string(node.LinkData.Destination)
+		r.doc.Links = append(r.doc.Links, str_dest)
+	case blackfriday.Image:
+		// pass
+	case blackfriday.Code:
+		r.doc.Code = append(r.doc.Code, str_value)
+	case blackfriday.Document:
+		break
+	case blackfriday.Paragraph:
+		// WHAT - PLEASE FIX ME...
+	case blackfriday.BlockQuote:
+		// WHAT - PLEASE FIX ME...
+	case blackfriday.HTMLBlock:
+		// WHAT - PLEASE FIX ME...
+	case blackfriday.Heading:
+
+		if !entering {
+			// WHAT
+		}
+	case blackfriday.HorizontalRule:
+		// pass
+	case blackfriday.List:
+		// WHAT
+	case blackfriday.Item:
+		// WHAT
+	case blackfriday.CodeBlock:
+		r.doc.Code = append(r.doc.Code, str_value)
+	case blackfriday.Table:
+		// pass
+	case blackfriday.TableCell:
+		if entering {
+
+			if node.Prev == nil {
+				// WHAT
+			}
+
+		} else {
+			// WHAT
+		}
+	case blackfriday.TableHead:
+		// pass
+	case blackfriday.TableBody:
+		// pass
+	case blackfriday.TableRow:
+		// pass
+	default:
+		panic("Unknown node type " + node.Type.String())
+	}
+	return blackfriday.GoToNext
+}
+
+func (r *SearchRenderer) RenderHeader(w io.Writer, ast *blackfriday.Node) {
+	return
+}
+
+func (r *SearchRenderer) RenderFooter(w io.Writer, ast *blackfriday.Node) {
+	// write to search index here...
+	return
 }
