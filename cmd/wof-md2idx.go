bbbc0f6ea43c15ff8e694deb3790341f70027ae6
thisisaaronland <devnull@localhost>
2019-04-08 17:08:16 -0700

    update default template

--- cmd/wof-md2idx.go
+++ cmd/wof-md2idx.go
@@ -42,13 +42,15 @@ func init() {
 
 {{$lena := len $fm.Authors }}
 {{$lent := len $fm.Tags }}
-<small style="font-style:italic;display:block;margin-bottom:2em;">This is a blog post by
-{{ range $ia, $a := $fm.Authors }}{{ if gt $lena 1 }}{{if eq $ia 0}}{{else if eq (plus1 $ia) $lena}} and {{else}}, {{end}}{{ end }}<a href="#" class="hey-look">{{ $a }}</a>{{ end }}
-{{ if gt $lent 0 }} that is tagged {{ range $it, $t := .Tags }}{{ if gt $lent 1 }}{{if eq $it 0}}{{else if eq (plus1 $it) $lent}} and {{else}}, {{end}}{{ end }}<a href="#" class="hey-look">{{ $t }}</a>{{ end }}.{{end}}
-{{ if $fm.Date }} It was published on <span class="pubdate"><a href="/blog/{{ $fm.Date.Year }}/{{ $fm.Date.Format "01" }}/">{{ $fm.Date.Format "January" }}</a> <a href="/blog/{{ $fm.Date.Year }}/{{ $fm.Date.Format "01" }}/{{ $fm.Date.Format "02" }}">{{ $fm.Date.Format "02"}}</a>, <a href="/blog/{{ $fm.Date.Year }}/">{{ $fm.Date.Format "2006" }}</a></span>.{{ end }}
+<small class="this-is">This is a blog post by
+    {{ range $ia, $a := $fm.Authors }}{{ if gt $lena 1 }}{{if eq $ia 0}}{{else if eq (plus1 $ia) $lena}} and {{else}}, {{end}}{{ end }}[{{ $a }}](/blog/authors/{{ prune_string $a  }}){{ end }}.
+    {{ if $fm.Date }}It was published on <span class="pubdate"><a href="/blog/{{ $fm.Date.Year }}/{{ $fm.Date.Format "01" }}/">{{ $fm.Date.Format "January" }}</a> <a href="/blog/{{ $fm.Date.Year }}/{{ $fm.Date.Format "01" }}/{{ $fm.Date.Format "02" }}/">{{ $fm.Date.Format "02"}}</a>, <a href="/blog/{{ $fm.Date.Year }}/">{{ $fm.Date.Format "2006" }}</a></span>{{ if gt $lent 0 }} and tagged {{ range $it, $t := $fm.Tags }}{{ if gt $lent 1 }}{{if eq $it 0}}{{else if eq (plus1 $it) $lent}} and {{else}}, {{end}}{{ end }}[{{ $t }}](/blog/tags/{{ prune_string $t  }}){{ end }}{{ end}}.
+    {{ else }}
+    It was tagged {{ range $it, $t := $fm.Tags }}{{ if gt $lent 1 }}{{if eq $it 0}}{{else if eq (plus1 $it) $lent}} and {{else}}, {{end}}{{ end }}[{{ $t }}](/blog/tags/{{ prune_string $t  }}){{ end }}.
+    {{ end }}
 </small>
-
-	    {{ end }}`
+{{ end }}
+{{ end }}`
 }
 
 type MarkdownOptions struct {
@@ -154,8 +156,6 @@ func GatherPosts(ctx context.Context, root string, html_opts *render.HTMLOptions
 			// SEE THIS? WE'RE "RECURSING"
 
 			if md_opts.Mode == "date" {
-
-				log.Println("WOOP WOOP")
 
 				i := filepath.Join(path, html_opts.Input)
 				_, err := os.Stat(i)
